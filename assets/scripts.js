9d872fbb152a03942d8cdf9a19ca988c8d169ddf
Lucas Long <73960771+motherocelot@users.noreply.github.com>
2025-11-21 18:19:26 -0800

    New Working Portfolio Site

---
+++ assets/scripts.js
@@ -0,0 +1,8 @@
+// script.js
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  hamburger.classList.toggle('toggle');
+  });
